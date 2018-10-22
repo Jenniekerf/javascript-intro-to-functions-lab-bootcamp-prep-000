2a7b5a66e27a6643c995cc0eb104d0a87a248dca
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-22 21:19:34 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ function logWhisper(string) {
   console.log(string.toLowerCase())
 }
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase('hello')) {
+  if (string === 'hello') {
     return 'I can\'t hear you';
   }
   if (string === string.toUpperCase()) {
