90b94ca17675870db218eb1e8ea02738d5fa7797
Jennie Kerfstedt <jenniekerf@gmail.com>
2018-10-22 20:59:35 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ function sayHiToGrandma(string) {
     return "I can\'t hear you";
   }
   if (string.toUpperCase() === string) {
-    return "YES INDEED";
+    return "YES INDEED!";
   }
-  if (string.to)
+  if (string === "I love you grandma");
+  return "I love you too";
 }
